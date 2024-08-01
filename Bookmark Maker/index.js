2c972d012fb895f0bef3502df32bbbe8240ff4d8
Rahul A <154581315+Rahuleey@users.noreply.github.com>
2024-08-01 21:37:52 +0530

    Update index.js

--- Bookmark Maker/index.js
+++ Bookmark Maker/index.js
@@ -1,20 +1,58 @@
-let defuserEl = document.getElementById("defuser");
-let timeEl = document.getElementById("timer");
-let count = 10;
+// Example Valid URLs: https://learning.ccbp.in/, https://www.google.com/
+let form = document.getElementById("bookmarkForm");
+let nameInput = document.getElementById("siteNameInput");
+let urlInput = document.getElementById("siteUrlInput");
 
-let timeReduce = setInterval(function() {
-    count -= 1;
-    timeEl.textContent = count;
-    if (count === 0) {
-        timeEl.textContent = "BOOM";
-        clearInterval(timeReduce);
+let btn = document.getElementById("submitBtn");
+let nameErr = document.getElementById("siteNameErrMsg");
+let urlErr = document.getElementById("siteUrlErrMsg");
+
+let bookmark = document.getElementById("bookmarksList");
+
+
+function createValues() {
+    let siteName = nameInput.value;
+    let siteUrl = urlInput.value;
+    let list = document.createElement("li");
+    bookmark.appendChild(list);
+
+    let title = document.createElement("p");
+    title.textContent = siteName;
+    title.style.fontWeight = "bold";
+    title.style.color = "black";
+    list.appendChild(title);
+
+    let link = document.createElement("a");
+    link.href = siteUrl;
+    link.textContent = siteUrl;
+    link.style.fontWeight = "600";
+    link.style.marginBottom = "10px";
+    link.setAttribute("target", "_blank");
+    bookmark.classList.toggle("d-none");
+    list.appendChild(link);
+}
+
+nameInput.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        nameErr.textContent = "Required*";
+    } else {
+        nameErr.textContent = "";
     }
-}, 1000);
+});
 
-defuserEl.addEventListener("keydown", function(event) {
-    let userText = defuserEl.value;
-    if (event.key === "Enter" && userText === "defuse" && count !== 0) {
-        timeEl.textContent = "You did it!";
-        clearInterval(timeReduce);
+urlInput.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        urlErr.textContent = "Required*";
+    } else {
+        urlErr.textContent = "";
+    }
+});
+
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    if (nameInput.value !== "" && urlInput.value !== "") {
+        bookmark.classList.add("bookmark-cont");
+        createValues();
     }
 });
