db712100ac1839ef496dfb98682f3a5415c26437
Rahul A <154581315+Rahuleey@users.noreply.github.com>
2024-08-01 21:03:41 +0530

    Update index.js

--- Countries Search Page/index.js
+++ Countries Search Page/index.js
@@ -1,20 +1,71 @@
-let defuserEl = document.getElementById("defuser");
-let timeEl = document.getElementById("timer");
-let count = 10;
+let inputEl = document.getElementById("searchInput");
+let result = document.getElementById("resultCountries");
+let spinnerEl = document.getElementById("spinner");
 
-let timeReduce = setInterval(function() {
-    count -= 1;
-    timeEl.textContent = count;
-    if (count === 0) {
-        timeEl.textContent = "BOOM";
-        clearInterval(timeReduce);
-    }
-}, 1000);
+let searchVal = "";
+let countries = [];
 
-defuserEl.addEventListener("keydown", function(event) {
-    let userText = defuserEl.value;
-    if (event.key === "Enter" && userText === "defuse" && count !== 0) {
-        timeEl.textContent = "You did it!";
-        clearInterval(timeReduce);
+function createSearch(country) {
+    let countryEl = document.createElement("div");
+    countryEl.classList.add("country-card", "col-11", "col-md-5", "mr-auto", "ml-auto", "d-flex", "flex-row");
+    result.appendChild(countryEl);
+
+    let img = document.createElement("img");
+    img.src = country.flag;
+    img.classList.add("country-flag", "mt-auto", "mb-auto");
+    countryEl.appendChild(img);
+
+    let content = document.createElement("div");
+    content.classList.add("d-flex", "flex-column", "ml-4");
+    countryEl.appendChild(content);
+
+    let name = document.createElement("p");
+    name.textContent = country.name;
+    name.classList.add("countr-name");
+    content.appendChild(name);
+
+    let population = document.createElement("p");
+    population.textContent = country.population;
+    population.classList.add("country-population");
+    content.appendChild(population);
+}
+
+
+function displaySearch() {
+    result.textContent = "";
+    for (let country of countries) {
+        let countryName = country.name;
+        if (countryName.toLowerCase().includes(searchVal.toLowerCase())) {
+            createSearch(country);
+        }
     }
-});
+}
+
+
+
+function getCountries() {
+    let url = "https://apis.ccbp.in/countries-data";
+    let options = {
+        method: "GET",
+    };
+
+    spinnerEl.classList.remove("d-none");
+    fetch(url, options)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(jsonData) {
+            spinnerEl.classList.add("d-none");
+            countries = jsonData;
+            displaySearch();
+        })
+}
+
+
+function changeSearch(event) {
+    searchVal = event.target.value;
+    displaySearch();
+}
+
+getCountries();
+inputEl.addEventListener("keyup", changeSearch);
