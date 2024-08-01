b82e443d31e27aec34059a75a8f332fb3235a415
Rahul A <154581315+Rahuleey@users.noreply.github.com>
2024-08-01 21:57:02 +0530

    Update index.js

--- Library Management/index.js
+++ Library Management/index.js
@@ -1,20 +1,59 @@
-let defuserEl = document.getElementById("defuser");
-let timeEl = document.getElementById("timer");
-let count = 10;
+let searchInp = document.getElementById("searchInput");
+let searchRes = document.getElementById("searchResults");
+let messageEl = document.getElementById("message");
+let spinnerEl = document.getElementById("spinner");
 
-let timeReduce = setInterval(function() {
-    count -= 1;
-    timeEl.textContent = count;
-    if (count === 0) {
-        timeEl.textContent = "BOOM";
-        clearInterval(timeReduce);
-    }
-}, 1000);
+let headEl = document.createElement("h1");
 
-defuserEl.addEventListener("keydown", function(event) {
-    let userText = defuserEl.value;
-    if (event.key === "Enter" && userText === "defuse" && count !== 0) {
-        timeEl.textContent = "You did it!";
-        clearInterval(timeReduce);
+function createSearch(search_results) {
+    if (searchInp.value.length < 1) {
+        messageEl.textContent = "No results found";
+        messageEl.classList.add("sub-head");
+        searchRes.textContent = "";
+        headEl.textContent = "";
+    } else {
+        messageEl.textContent = "";
+        headEl.textContent = "Popular Books";
+        headEl.classList.add("sub-head");
+        searchRes.appendChild(headEl);
+        for (let item of search_results) {
+            let title = item.title;
+            let img = item.imageLink;
+            console.log(item.title);
+            let author = item.author;
+            let cont = document.createElement("div");
+            cont.classList.add("col-6");
+            let imgEl = document.createElement("img");
+            let textEl = document.createElement("p");
+            imgEl.setAttribute("src", img);
+            imgEl.classList.add("book-img");
+            textEl.textContent = author;
+            cont.appendChild(imgEl);
+            cont.appendChild(textEl);
+            searchRes.appendChild(cont);
+        }
     }
-});
+}
+
+searchInp.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        spinnerEl.classList.toggle("d-none");
+        let val = searchInp.value;
+        let url = "https://apis.ccbp.in/book-store?title=" + val;
+        let options = {
+            method: "GET"
+        };
+        fetch(url, options)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(jsonData) {
+                let {
+                    search_results
+                } = jsonData;
+                console.log(jsonData)
+                createSearch(search_results);
+                spinnerEl.classList.toggle("d-none");
+            });
+    }
+})
