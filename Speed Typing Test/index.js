c7864658bbdf961abe32e919b27f79aa748df571
Rahul A <154581315+Rahuleey@users.noreply.github.com>
2024-09-20 21:09:12 +0530

    Update index.js

--- Speed Typing Test/index.js
+++ Speed Typing Test/index.js
@@ -1,20 +1,53 @@
-let defuserEl = document.getElementById("defuser");
-let timeEl = document.getElementById("timer");
-let count = 10;
+let mainEl = document.getElementById("speedTypingTest");
+let timerEl = document.getElementById("timer");
+let quoteDisplayEl = document.getElementById("quoteDisplay");
+let resultEl = document.getElementById("result");
+let quoteInputEl = document.getElementById("quoteInput");
+let submitBtnEl = document.getElementById("submitBtn");
+let resetBtnEl = document.getElementById("resetBtn");
+let spinnerEl = document.getElementById("spinner");
+let counter = 0;
 
-let timeReduce = setInterval(function() {
-    count -= 1;
-    timeEl.textContent = count;
-    if (count === 0) {
-        timeEl.textContent = "BOOM";
-        clearInterval(timeReduce);
-    }
-}, 1000);
+spinnerEl.classList.toggle("d-none");
 
-defuserEl.addEventListener("keydown", function(event) {
-    let userText = defuserEl.value;
-    if (event.key === "Enter" && userText === "defuse" && count !== 0) {
-        timeEl.textContent = "You did it!";
-        clearInterval(timeReduce);
+function startCounter() {
+    counter += 1;
+    timerEl.textContent = counter;
+}
+let counterVal = setInterval(startCounter, 1000);
+
+function getQuote() {
+    let options = {
+        method: "GET"
     }
-});
+    fetch("https://apis.ccbp.in/random-quote", options)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(jsonData) {
+            spinnerEl.classList.add("d-none");
+            let quote = jsonData.content;
+            quoteDisplayEl.textContent = quote;
+        });
+}
+getQuote();
+startCounter();
+
+
+resetBtnEl.onclick = function() {
+    spinnerEl.classList.remove("d-none");
+    counter = 0;
+    getQuote();
+    startCounter();
+    resultEl.textContent = "";
+    quoteInputEl.textContent = "";
+}
+
+submitBtnEl.onclick = function() {
+    if (quoteInputEl.value === quoteDisplayEl.textContent) {
+        clearInterval(counterVal);
+        resultEl.textContent = "You typed in " + counter + " seconds";
+    } else {
+        resultEl.textContent = "You typed incorrect sentence";
+    }
+}
